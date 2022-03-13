a3b5c8423e0ec6271edd4981c529e82208079731
Gui-dev <adrianon10@gmail.com>
2022-03-13 19:51:24 -0300

    feat: add cors

--- src/App.ts
+++ src/App.ts
@@ -1,8 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 const PORT = 3333 || process.env.PORT
 
+app.use(cors())
+
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta: ${PORT}`)
+  console.log(`🚀 Servidor rodando na porta: ${PORT}`)
 })
