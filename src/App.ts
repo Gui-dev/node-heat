c13c2e9f94ef2bfda2516deaca86b60db9dc641a
Gui-dev <adrianon10@gmail.com>
2022-03-16 03:20:13 -0300

    feat: add dotenv and express.json()

--- src/App.ts
+++ src/App.ts
@@ -1,10 +1,16 @@
+import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+
+import { routes } from 'routes'
 
 const app = express()
 const PORT = 3333 || process.env.PORT
 
 app.use(cors())
+app.use(express.json())
+
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta: ${PORT}`)
