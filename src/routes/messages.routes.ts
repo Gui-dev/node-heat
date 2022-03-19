263e287877fe41eb71614cfa46bafeecbcfb0ab1
Gui-dev <adrianon10@gmail.com>
2022-03-19 01:43:47 -0300

    feat: add new route

--- src/routes/messages.routes.ts
+++ src/routes/messages.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 
 import { CreateMessageController } from '@controllers/CreateMessageController'
+import { GetLastMessagesController } from '@controllers/GetLastMessagesController'
 import { ensureAuthenticate } from '@middlewares/ensureAuthenticate'
 
 const routerMessages = Router()
 const createMessageController = new CreateMessageController()
+const getLastMessagesController = new GetLastMessagesController()
 
+routerMessages.get('/last-messages', getLastMessagesController.index)
 routerMessages.post('/', ensureAuthenticate, createMessageController.create)
 
 export { routerMessages }
